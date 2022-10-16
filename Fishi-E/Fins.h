8fa71619b1e0df72b3cc24b5a80cde79449c8353
DethCubeHax <ultimate.nafis.bhadra11@gmail.com>
2022-10-16 21:54:39 +0800

    Optimised fin control function

--- Fishi-E/Fins.h
+++ Fishi-E/Fins.h
@@ -24,37 +24,35 @@ class Fins
     
     void finControl()
     {
-//        s1 = (((43-rollVal)*maxAttackAngle/40 +(42-pitchVal)*maxAttackAngle/40) + 90.0);
-//        s2 = (((43-rollVal)*maxAttackAngle/40 -(42-pitchVal)*maxAttackAngle/40) + 90.0);
-//        s1 = constrain(s1, 0, 180);
-//        s2 = constrain(s2, 0, 180);
-//        servo1.write(s1);
-//        servo2.write(s2);
-//        #if debug
-//        Serial.println((String) "S1:" + s1);
-//        Serial.println((String) "S2:" + s2);
-//        #endif
-
         //For servo used, the range of writeMicroseconds are different
-        s1 = (((rollVal)*maxAttackAngle/80 +(pitchVal)*maxAttackAngle/80) + 90.0) + servoOneCalibration;
-        s2 = (((rollVal)*maxAttackAngle/80 -(pitchVal)*maxAttackAngle/80) + 90.0) + servoTwoCalibration;
-        s1 = constrain(s1, 25, 155);
-        s2 = constrain(s2, 25, 155);
-        // The range of DS3120: 500 - 2500 (red 20kg servo)
-        s1 = map(s1, 0, 180, 700, 2200); 
-        s2 = map(s2, 0, 180, 700, 2200);
-        s1 = constrain(s1, 700, 2200);
-        s2 = constrain(s2, 700, 2250);
+        s1 = getServoMicroseconds(1, rollVal, pitchVal);
+        s2 = getServoMicroseconds(-1, rollVal, pitchVal);
 
-//         servo1.write(s1);
-//         servo2.write(s2);
-        
-//        int test = 0;
-//        servo1.write(test);
-//        servo2.write(90);
         servo1.writeMicroseconds(s1);
         servo2.writeMicroseconds(s2);
         
         //Serial.println((String) "S1: " + s1 + " S2: " + s2 + " Roll: " + rollVal + " pitch: " + pitchVal);
+    }
+
+
+    private:
+    int getServoMicroseconds(int side, int rollVal, int pitchVal)
+    {
+        //        s1 = (((43-rollVal)*maxAttackAngle/40 +(42-pitchVal)*maxAttackAngle/40) + 90.0);
+        //        s2 = (((43-rollVal)*maxAttackAngle/40 -(42-pitchVal)*maxAttackAngle/40) + 90.0);
+        //        s1 = constrain(s1, 0, 180);
+        //        s2 = constrain(s2, 0, 180);
+        //        servo1.write(s1);
+        //        servo2.write(s2);
+        //        #if debug
+        //        Serial.println((String) "S1:" + s1);
+        //        Serial.println((String) "S2:" + s2);
+        //        #endif
+
+        float x = (((rollVal)*maxAttackAngle/80 + (side*pitchVal)*maxAttackAngle/80) + 90.0) + servoOneCalibration;
+        x = constrain(x, 25, 155);
+        x = map(x, 0, 180, 700, 2200);
+        x = constrain(x, 700, 2200);
+        return int(x);
     }
 };
